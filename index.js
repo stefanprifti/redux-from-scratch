e1032f39cf92a34d80a413a0f4116677264a1b9c
Stefan Prifti <s.prifti.al@gmail.com>
2018-04-19 16:27:15 +0200

    test another export

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-export * from "./react"
-export * from "./react-redux"
+import { combineReducers, createStore, bindActionCreators, applyMiddleware } from "./redux"
+import { Provider, connect } from "./react-redux";
+
+export {
+  combineReducers,
+  createStore,
+  bindActionCreators,
+  applyMiddleware,
+  Provider,
+  connect
+}
